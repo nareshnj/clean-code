e2c3e35ad5ece725b80b048a8916abe62aa17f5d
naresh <naresh.jagadale@gmail.com>
2021-02-06 14:32:34 +0530

    refactor test cases

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
@@ -1,44 +1,46 @@
 package com.a.introduction.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
-public class GildedRoseADefaultItemTest {
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 1 when the item is not expired
-	 * and sell in should decrease by 1.
-	 * 
-	 */
-	@Test
-	public void testUpdateQualityDefault1() {
-		Item item = new Item("DEFAULT_ITEM", 15, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items[0].name);
-		assertEquals(14, app.items[0].sellIn);
-		assertEquals(2, app.items[0].quality);
-	}
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 2 when the item is expired(Sell in  < 0) and sell in should decrease by 1.
-	 * 
-	 */
-	@Test
-	public void testUpdateQualityForExpiredItem() {
-		Item item = new Item("DEFAULT_ITEM", -1, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items[0].name);
-		assertEquals(-2, app.items[0].sellIn);
-		assertEquals(1, app.items[0].quality);
-	}
+public class GildedRoseADefaultItemTest {
+    private static final int NOT_EXPIRED_SELLIN = 15;
+    private static final int EXPIRED_SELLIN = -1;
+    private static final int QUALITY = 3;
+    public static final String DEFAULT_ITEM = "DEFAULT_ITEM";
+
+    @Test
+    public void test_unexpiredDefaultItem_qualityDecreaseBy1() {
+        GildedRose app = createGildedRoseWithItem(DEFAULT_ITEM, NOT_EXPIRED_SELLIN, QUALITY);
+
+        app.updateQuality();
+        Item expectedItem = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELLIN - 1, QUALITY - 1);
+
+        assertItem(expectedItem, app.items[0]);
+    }
+
+    @Test
+    public void test_expiredDefaultItem_qualityDecreaseBy2() {
+        GildedRose app = createGildedRoseWithItem(DEFAULT_ITEM, EXPIRED_SELLIN, QUALITY);
+
+        app.updateQuality();
+        Item expectedItem = new Item(DEFAULT_ITEM, EXPIRED_SELLIN - 1, QUALITY - 2);
+
+        assertItem(expectedItem, app.items[0]);
+
+    }
+
+    private GildedRose createGildedRoseWithItem(String defaultItem, int notExpiredSellin, int quality) {
+        Item item = new Item(defaultItem, notExpiredSellin, quality);
+        Item[] items = new Item[]{item};
+        return new GildedRose(items);
+    }
+
+    private void assertItem(Item expectedItem, Item actualItem) {
+        assertEquals(expectedItem.name, actualItem.name);
+        assertEquals(expectedItem.sellIn, actualItem.sellIn);
+        assertEquals(expectedItem.quality, actualItem.quality);
+    }
+
 }
